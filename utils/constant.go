8eff38bdb13ff9297905b5b3690f4fea661bd5da
Jonas Kaninda <jonaskaninda@gmail.com>
2024-01-20 13:09:15 +0100

    docs: update readme

--- utils/constant.go
+++ utils/constant.go
@@ -2,8 +2,8 @@ package utils
 
 const Notice = "Please remove --operation flag.\n" +
 	"Use: \n" +
-	"- backup for backup operation [eg: bkup backup -d  database_name ...]\n" +
-	"- restore for restore operation [eg. bkup restore -d database_name ...]\n" +
+	"- backup for database backup operation [eg: bkup backup -d  database_name ...]\n" +
+	"- restore for database restore operation [eg. bkup restore -d database_name ...]\n" +
 	"We are sorry for this inconvenient"
 const RestoreExample = "mysql-bkup restore --dbname database --file db_20231219_022941.sql.gz\n" +
 	"bkup restore --dbname database --storage s3 --path /custom-path --file db_20231219_022941.sql.gz"
